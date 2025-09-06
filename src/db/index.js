513ea442c7403a565aabf5a873654b78a853d3cb
abhi11082005 <abhiguptaultimate2005@gmail.com>
2025-09-06 23:22:45 +0530

    Update index.js

--- src/db/index.js
+++ src/db/index.js
@@ -5,7 +5,7 @@ const connectDB=async()=>{
     try{
         
         const connectionInstance=await mongoose.connect
-        (`${process.env.MONGODB_URI}/${DB_NAME}`);
+        (`${process.env.MONGODB_URI}/${process.env.DB_NAME}`);
         console.log(`MongoDB connected succesfully!! on Host:${connectionInstance.connection.host}`)
     }
     catch(error){
